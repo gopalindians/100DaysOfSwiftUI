f1051e7201e13d0b9ceb58c4005bb41730c38089
Gopal Sharma <gopalindians@gmail.com>
2023-09-03 22:31:51 +0530

    Update 001.swift
    //001 – Day 001 – variables, constants, strings, and numbers

--- 001.swift
+++ 001.swift
@@ -1,3 +1,26 @@
 import SwiftUI
-//000 – Day 001 – variables, constants, strings, and numbers
+//001 – Day 001 – variables, constants, strings, and numbers
+
+
+var greeting = "hello, playground"
+var name = "Ted"
+name = "Red"
+name = "kelly"
+
+
+let character = "D";
+
+//character = "E"
+//character = "F"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+
+let managerName = "M"
+let dogBreed = "S"
+let meaningOfLife = "m"
 
