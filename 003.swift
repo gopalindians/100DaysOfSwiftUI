a660860f5fd38533be841372cc6a9abf1472a5e1
Gopal Sharma <gopalindians@gmail.com>
2023-09-17 09:41:58 +0530

    Update 003.swift
    //2 How to store and find data in dictionaries

--- 003.swift
+++ 003.swift
@@ -65,3 +65,68 @@ print(reversedPresidents)
 
 
 
+
+
+//2 How to store and find data in dictionaries
+
+var employee = ["Taylor Swift", "Singer", "Nashville"]
+
+
+print("Name: \(employee[0])")
+print("Job title: \(employee[1])")
+print("Location: \(employee[2])")
+
+print("Name: \(employee[0])")
+employee.remove(at: 1)
+print("Job title: \(employee[1])")
+print("Location: \(employee[2])")
+
+let employee2 = ["name": "Taylor Swift", "job": "Singer", "location": "Nashville"]
+
+let employee3 = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
+
+
+print(employee3["name"])
+print(employee3["job"])
+print(employee3["location"])
+
+print(employee2["password"])
+print(employee2["status"])
+print(employee2["manager"])
+
+print(employee2["name", default: "Unknown"])
+print(employee2["job", default: "Unknown"])
+print(employee2["location", default: "Unknown"])
+
+
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false,
+]
+
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2021: "Tokyo"
+]
+
+print(olympics[2012, default: "Unknown"])
+
+
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Shaquille O'Neal"] = 216
+heights["LeBron James"] = 206
+
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+
+archEnemies["Batman"] = "Penguin"
